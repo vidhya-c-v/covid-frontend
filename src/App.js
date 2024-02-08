dbd3d3743eaccdffca04fec33e44cfd1f1bc4098
vidhya-c-v <vidhyacv14@gmail.com>
2024-02-09 01:30:37 +0530

    add-done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Add from './components/Add';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<NavBar/>}/>
+        <Route path='/' element={<Add/>}/>
       </Routes>
       </BrowserRouter>
       
